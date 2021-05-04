cc20a28ad0af67c36783f40a51a87159a1182f87
Maksim <maksimshalaev11@gmail.com>
2021-05-04 11:36:22 +0300

    2

--- BmiService.java
+++ BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-   public float calculate (float weight){
-    // ввести рост в метрах!!
-    float height = 1.72F;
+    public float calculate(float weight) {
+        // ввести рост в метрах!!
+        float height = 1.72F;
         final float i = weight / (height * height);
         return i;
 

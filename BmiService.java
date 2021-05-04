5d7fd1f0b03afa417d3b36c867fad89403531d82
Maksim <maksimshalaev11@gmail.com>
2021-05-04 14:23:08 +0300

    3

--- BmiService.java
+++ BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
-    public float calculate(float weight) {
-        // ввести рост в метрах!!
-        float height = 1.72F;
+    public float calculate(float weight, float height) {
         final float i = weight / (height * height);
         return i;
 
--- MainV4.java
+++ MainV4.java
@@ -2,8 +2,10 @@ public class MainV4 {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         // ввести вес в кг
-        float weight = 90.1F;
-        float index = service.calculate(weight);
+        float weight = 98.1F;
+        // ввести рост в метрах
+        float height = 1.42F;
+        float index = service.calculate(weight, height);
         System.out.println("Индекс массы тела:" + " " + index);
     }
 }
